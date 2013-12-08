7f261dc1f9a368baf14a3bc38bac92790b0ccbdf
dawiddrelichowski <dawiddrelichowski@vm-0.dawiddrelichowski.koding.kd.io>
2013-12-08 18:59:01 +0000

    Changes in tests

--- js/tests/spec/sandbox.js
+++ js/tests/spec/sandbox.js
@@ -1,13 +1,34 @@
 define(['chai', 'sandbox'], function(chai, Sandbox) {
     describe('Sandbox', function() {
-        describe('#trigger', function() {
-            it('should return Sandbox', function() {
-                chai.expect((new Sandbox()).trigger('test.event')).to.be.instanceof(Sandbox);
-            });
+        var sandbox;
+        
+        beforeEach(function() {
+            sandbox = new Sandbox();
         });
+        
         describe('#on', function() {
             it('should return Sandbox', function() {
-                chai.expect((new Sandbox()).on('test.event')).to.be.instanceof(Sandbox);
+                chai.expect(sandbox.on('test.event')).to.be.instanceof(Sandbox);
+            });
+        });
+        
+        describe('#trigger', function() {
+            it('should return Sandbox', function() {
+                chai.expect(sandbox.trigger('test.event')).to.be.instanceof(Sandbox);
+            });
+            
+            it('should call callback set in #on', function(done) {
+                sandbox.on('test.event', function(data) {
+                    chai.expect(data).to.deep.equal({name: 'Patricia'});
+                });
+                
+                sandbox.trigger('test.event', {name: 'Patricia'});
+                done();
+            });         
+        });
+        describe('#dragAndDropSupported', function() {
+            it('should return true', function() {
+                chai.expect(sandbox.dragAndDropSupported()).to.be.true;
             });
         });
     });
